635fb104214e16a52b32e0a0b92ae0694fd8c6cd
cindyhu2023 <cindyhu2023@u.northwestern.edu>
2023-05-14 14:04:39 -0500

    change tab to dropdown

--- src/Pages/OneRMDashboard.jsx
+++ src/Pages/OneRMDashboard.jsx
@@ -1,15 +1,52 @@
-import data from "../../data.json";
 import data_1RM from "../../data_1RM.json";
 import { useState , useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import CreateGraph from "../Utils/CreateGraph";
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { set } from "firebase/database";
+import Button from '@mui/material/Button';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 function OneRMDashboard() {
     let navigate = useNavigate();
     const [pointsToGraph, setPointsToGraph] = useState([]);
-    const [alignment, setAlignment] = useState('');
+
+    const options = [
+        'Bench Press',
+        'Squat',
+        'Deadlift',
+        'Military Press',
+        'Barbell Row',
+        'Front Squat'
+      ];
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [alignment, setAlignment] = useState(options[selectedIndex]);
+    const open = Boolean(anchorEl);
+
+    useEffect(() => {
+        setAlignment(options[selectedIndex]);
+    }, [selectedIndex]);
+
+    useEffect(() => {
+        handleChange(null, alignment);
+    }, [alignment]);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuItemClick = (event, index) => {
+        setSelectedIndex(index);
+        setAnchorEl(null);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+  
 
     const handleChange = (event, newAlignment) => {
         if(newAlignment === null){
@@ -48,20 +85,40 @@ function OneRMDashboard() {
             <h2>This is your progress for your estimated 1 rep maxes</h2>
             <h3>Select your exercise here:</h3>
             <br></br>
-            <ToggleButtonGroup
-                color="primary"
-                value={alignment}
-                exclusive
-                onChange={handleChange}
-                aria-label="Platform"
-                >
-                <ToggleButton value="Bench Press">Bench Press</ToggleButton>
-                <ToggleButton value="Squat">Squat</ToggleButton>
-                <ToggleButton value="Deadlift">Deadlift</ToggleButton>
-                <ToggleButton value="Military Press">Military Press</ToggleButton>
-                <ToggleButton value="Barbell Row">Barbell Row</ToggleButton>
-                <ToggleButton value="Front Squat">Front Squat</ToggleButton>
-            </ToggleButtonGroup>
+            <div>
+            <Button
+            id="demo-customized-button"
+                aria-controls={open ? 'demo-customized-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                variant="outlined"
+                disableElevation
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDownIcon />}
+            >
+                {options[selectedIndex]}
+            </Button>
+                    <Menu
+                        id="lock-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                        'aria-labelledby': 'lock-button',
+                        role: 'listbox',
+                        }}
+                    >
+                        {options.map((option, index) => (
+                        <MenuItem
+                            key={option}
+                            selected={index === selectedIndex}
+                            onClick={(event) => handleMenuItemClick(event, index)}
+                        >
+                            {option}
+                        </MenuItem>
+                        ))}
+                    </Menu>
+            </div>
             <div>
                 <h3>This is your estimated 1 rep max progress for {alignment}: </h3>
                 <div id="graph">
--- src/Pages/WorkoutVolumeDashboard.jsx
+++ src/Pages/WorkoutVolumeDashboard.jsx
@@ -4,11 +4,47 @@ import { useNavigate } from "react-router-dom";
 import CreateGraph from "../Utils/CreateGraph";
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import Button from '@mui/material/Button';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 function WorkoutVolumeDashboard() {
+    const options = [
+        'Push 1',
+        'Pull 1',
+        'Legs 1',
+        'Push 2',
+        'Pull 2',
+        'Legs 2'
+      ];
     let navigate = useNavigate();
     const [pointsToGraph, setPointsToGraph] = useState([]);
-    const [alignment, setAlignment] = useState('');
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [alignment, setAlignment] = useState(options[selectedIndex]);
+    const open = Boolean(anchorEl);
+
+    useEffect(() => {
+        setAlignment(options[selectedIndex]);
+    }, [selectedIndex]);
+
+    useEffect(() => {
+        handleChange(null, alignment);
+    }, [alignment]);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuItemClick = (event, index) => {
+        setSelectedIndex(index);
+        setAnchorEl(null);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     const handleChange = (event, newAlignment) => {
         if(newAlignment === null){
@@ -46,7 +82,41 @@ function WorkoutVolumeDashboard() {
             <h2>This is your progress for your workout volume</h2>
             <h3>Select your exercise here:</h3>
             <br></br>
-            <ToggleButtonGroup 
+            <div>
+            <Button
+            id="demo-customized-button"
+                aria-controls={open ? 'demo-customized-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                variant="outlined"
+                disableElevation
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDownIcon />}
+            >
+                {options[selectedIndex]}
+            </Button>
+                    <Menu
+                        id="lock-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                        'aria-labelledby': 'lock-button',
+                        role: 'listbox',
+                        }}
+                    >
+                        {options.map((option, index) => (
+                        <MenuItem
+                            key={option}
+                            selected={index === selectedIndex}
+                            onClick={(event) => handleMenuItemClick(event, index)}
+                        >
+                            {option}
+                        </MenuItem>
+                        ))}
+                    </Menu>
+            </div>
+            {/* <ToggleButtonGroup 
                 color="secondary"
                 value={alignment}
                 exclusive
@@ -59,7 +129,7 @@ function WorkoutVolumeDashboard() {
                 <ToggleButton value="Push 2">Push 2</ToggleButton>
                 <ToggleButton value="Pull 2">Pull 2</ToggleButton>
                 <ToggleButton value="Legs 2">Legs 2</ToggleButton>
-            </ToggleButtonGroup>
+            </ToggleButtonGroup> */}
             <div>
                 <h3>This is your estimated workout volume progress for {alignment}: </h3>
                 <div id="graph">
