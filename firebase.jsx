795266c70bda6b69c4ea5b08a64aae3b63db434b
cindyhu2023 <cindyhu2023@u.northwestern.edu>
2023-06-07 13:01:27 -0500

    remove database config

--- firebase.jsx
+++ firebase.jsx
@@ -23,32 +23,14 @@ const firebaseAppConfig = {
 };
 
 const firebaseDatabaseConfig = {
-  apiKey: "AIzaSyA7NAaL6Sl9sZ2gRoQi97w6J8-urpVX0nc",
-  authDomain: "olympus-app-9d635.firebaseapp.com",
-  databaseURL: "https://olympus-app-9d635-default-rtdb.firebaseio.com",
-  projectId: "olympus-app-9d635",
-  storageBucket: "olympus-app-9d635.appspot.com",
-  messagingSenderId: "526654239507",
-  appId: "1:526654239507:web:e49136e3a5746a8e1560e6",
-  measurementId: "G-BNY8WBLRG2"
+// your firebase config here
 };
 
 // Initialize Firebase for app
 export const app = firebase.initializeApp(firebaseAppConfig);
-export const dbApp = firebase.initializeApp(firebaseDatabaseConfig, "database");
 export const db = getDatabase(app);
 // const analytics = getAnalytics(app);
 const projectAuth = getAuth();;
-
-
-// if (process.env.NODE_ENV!== 'production') {
-//   connectAuthEmulator(projectAuth, "http://127.0.0.1:9099");
-//   connectFirestoreEmulator(db, "127.0.0.1", 8080);
-
-//   signInWithCredential(projectAuth, GoogleAuthProvider.credential(
-//     '{"sub": "XBfU5lqHTUjeFo9jYVoKz1hkGpAe", "email": "cindy@gmail.com", "displayName":"Cindy Hu", "email_verified": true}'
-//   ));
-// }
 
 
 export { projectAuth };
